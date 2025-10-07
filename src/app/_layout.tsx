ace80f7a275f5bd4468fcaa7032b06832cdcfb0b
Stephen barreto <stephen.barreto@sofka.com.co>
2025-10-07 18:52:48 -0500

    layoutr screen and iconst on screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,6 +6,10 @@ const RootLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Meditations" }} />
+      <Stack.Screen
+        name="meditation/[id]"
+        options={{ headerShown: false, animation: "slide_from_bottom" }}
+      />
     </Stack>
   );
 };
--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -1,25 +1,38 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { meditations } from "@/data";
-import { useLocalSearchParams } from "expo-router";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { router, useLocalSearchParams } from "expo-router";
 
 const MeditationDetailsScreen = () => {
   const { id } = useLocalSearchParams();
+
+  const { top } = useSafeAreaInsets();
 
   const meditation = meditations.find((meditation) => meditation.id === Number(id));
 
   if (!meditation) {
     return (
-      <View>
+      <SafeAreaView>
         <Text>Meditation not found</Text>
-      </View>
+      </SafeAreaView>
     );
   }
 
   return (
-    <View>
-      <Text>Meditation Details {meditation?.title}</Text>
-    </View>
+    <SafeAreaView>
+      <Text className=" text-2xl">Meditation Details {meditation?.title}</Text>
+      <AntDesign
+        onPress={() => router.back()}
+        className="absolute right-4"
+        name="close"
+        size={24}
+        style={{ marginTop: top + 10 }}
+        color="black"
+      />
+    </SafeAreaView>
   );
 };
 
